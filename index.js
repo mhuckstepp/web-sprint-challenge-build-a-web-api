91419d7eabb14882c5200fd0fcb15b7a4120e908
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-09 09:52:27 -0700

    installed dotenv for deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const dotenv = require("dotenv").config();
 const server = require("./api/server");
 
 const port = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(`listening on localhost:${port}`);
 });
